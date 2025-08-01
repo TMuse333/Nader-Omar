5ce4e1c2a36f186c6d357e38127990d49904d1dc
Thomas Musial <thomaslmusial@gmail.com>
2025-08-01 17:11:54 -0300

    moneky containment

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Analytics/>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
