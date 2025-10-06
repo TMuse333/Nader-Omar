29b6da3d510a8e45cf1a8d615eb1309c799bbdd8
Thomas Musial <thomaslmusial@gmail.com>
2025-10-06 16:54:45 -0300

    not limited

--- frontend/src/app/privacy/page.tsx
+++ frontend/src/app/privacy/page.tsx
@@ -3,9 +3,9 @@ import React from "react";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: "Nader Omar Limited | Privacy Policy",
+    title: "Nader Omar | Privacy Policy",
     description:
-      "Read the Privacy Policy for Nader Omar Limited, outlining how we collect, use, maintain, and disclose user information on the naderomarrealto.com website.",
+      "Read the Privacy Policy for Nader Omar, outlining how we collect, use, maintain, and disclose user information on the naderomarrealtor.com website.",
     keywords: [
       "Nader Omar Limited",
       "Privacy Policy",
@@ -14,21 +14,21 @@ export const metadata: Metadata = {
       "Fall River Nova Scotia Privacy"
     ],
     openGraph: {
-      title: "Nader Omar Limited | Privacy Policy",
+      title: "Nader Omar  | Privacy Policy",
       description:
-        "Learn about Nader Omar Limited's Privacy Policy, detailing the collection, use, and protection of user data on naderomarrealto.com.",
+        "Learn about Nader Omar Limited's Privacy Policy, detailing the collection, use, and protection of user data on naderomarrealtor.com.",
       url: "https://www.naderomarrealtor.com/privacy-policy",
       images: [
         {
           url: "https://naderomarrealtor.com/nader.png",
           width: 1200,
           height: 630,
-          alt: "Nader Omar Limited | Privacy Policy"
+          alt: "Nader Omar  | Privacy Policy"
         }
       ],
       type: "website",
       locale: "en_US",
-      siteName: "Nader Omar Limited | Privacy Policy"
+      siteName: "Nader Omar  | Privacy Policy"
     },
     icons: {
       icon: ["/favicon.ico?v=4"]
